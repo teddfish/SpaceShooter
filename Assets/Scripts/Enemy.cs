606fa1a2b95908f8dba4b327ffc464520d12d2f2
Shwet <shwetaketu1@gmail.com>
2021-05-18 23:35:38 +0530

    some more stuff

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * 4 * Time.deltaTime);
+
+        if (transform.position.y < -11f)
+        {
+            transform.position = new Vector3(Random.Range(-9f, 9f), 8f, transform.position.z);
+        }
+    }
+}
